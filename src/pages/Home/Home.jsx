2a01e6423695ae732c7e563924ec36e84f192b5e
Maksim Bekis <maxamax997@gmail.com>
2023-08-01 14:06:16 +0300

    add header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,17 @@
-export default function home(){
-  return(
-    <div>
-      Скоро начну делать
-    </div>
-  )
+import search from "./../../img/svg/search.svg";
+import st from "./Home.module.scss";
+export default function home() {
+  return (
+    <header className={st.header}>
+      <button className={st.buttonAdd}>+ Добавить вещь</button>
+      <h2 className={st.h2Name}>Мария Иванова</h2>
+      <div className={st.search}>
+        <input placeholder="Поиск" className={st.inputSearch} type="text" />
+        <button className={st.buttonSearch}>
+          <img src={search} />
+        </button>
+        <button className={st.buttonExit}>Выход</button>
+      </div>
+    </header>
+  );
 }
